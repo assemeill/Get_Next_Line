c964322eea92d84de8449619f915a5cb7e844d2a
Assem Zhilkibayeva <aszhilki@e1z2r7p11.42.us.org>
2019-11-01 14:00:36 -0700

    Checked and updated to avoid leaks

--- get_next_line.c
+++ get_next_line.c
@@ -2,37 +2,34 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void	ft_write(char *tmp, char **left, char **line)
+static void	ft_write(char **tmp, char **left, char **line)
 {
 	int i;
 
 	i = 0;
-	while (tmp[i] != '\n' && tmp[i] != '\0')
+	while ((*tmp)[i] != '\n' && (*tmp)[i] != '\0')
 		i++;
-	*line = ft_strsub(tmp, 0, i);
-	printf("%s\n", *line);
+	if (*line)
+		ft_strdel(line);
+	else
+		*line = NULL;
+	*line = ft_strsub(*tmp, 0, i);
 	i++;
-	if (tmp[i] != '\0' && tmp[i] != '\n')
+	if ((*tmp)[i] != '\0' && (*tmp)[i] != '\n')
 	{
 		if (*left)
 			ft_strdel(left);
-		*left = ft_strdup(&tmp[i]);
-//		if (!(*left))
-//			*left = ft_strdup(&tmp[i]);
-//		else if (**left == '\n')
-//		{
-//			(*left)++;
-//			*left = ft_strjoin(*left, &tmp[i]);
-//		}
+		*left = ft_strdup(*tmp + i);
 	}
+	ft_strdel(tmp);
 }
 
 int		ft_return(char *left, int number, char **line)
 {
 	if (number < 0)
-		return(-1);
+		return (-1);
 	else if ((number == 0) && !left && !(*line))
-		return(0);
+		return (0);
 	else
 		return (1);
 }
@@ -40,20 +37,21 @@ int		ft_return(char *left, int number, char **line)
 int		get_next_line(const int fd, char **line)
 {
 	static char	*left[FD_MAX];
+	char		*save;
 	char		*tmp;
 	char		buf[BUFF_SIZE + 1];
 	int			number;
 
 	tmp = NULL;
-	*line = NULL;
-	if ((read(fd, buf, 0) < 0) || !fd)
+	if ((read(fd, buf, 0) < 0) || (fd < 0 || line == NULL))
 		return (-1);
+	*line = NULL;
+	if (line)
+		ft_strdel(line);
 	if (left[fd])
 	{
-		tmp = left[fd];
+		tmp = ft_strdup(left[fd]);
 		ft_strdel(&left[fd]);
-//		if (ft_strchr(tmp, '\n'))
-//			left[fd] = ft_strdup(ft_strchr(tmp, '\n'));
 	}
 	while ((number = read(fd, buf, BUFF_SIZE)) > 0)
 	{
@@ -62,13 +60,16 @@ int		get_next_line(const int fd, char **line)
 			tmp = ft_strdup(buf);
 		else
 		{
-			//free mem
+			save = tmp;
 			tmp = ft_strjoin(tmp, buf);
+			ft_strdel(&save);
 		}
-		if (strchr(buf, '\n'))
+		if (ft_strchr(buf, '\n'))
 			break ;
 	}
 	if (tmp)
-		ft_write(tmp, &(left[fd]), line);
+		ft_write(&tmp, &(left[fd]), line);
+	// need to send pointer to tmp instead of creating local copy
 	return (ft_return(left[fd], number, line));
 }
+
